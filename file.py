4e129638f602c9190242bd7949cc7ed4c963c2ef
sargon <grynets1@gmail.com>
2017-04-17 23:25:49 +0300

    Added ball moving

--- file.py
+++ file.py
@@ -1,12 +1,19 @@
 from tkinter import *
-import random
+from Ball import *
 import time
 
 tk = Tk()
 tk.title("Game")
 tk.resizable(0, 0)
 tk.wm_attributes("-topmost", 1)
-canvas = Canvas(tk, width=500, height=500, bd=0, highlighthickness=0)
+canvas = Canvas(tk, width=500, height=500, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
 
+ball = Ball(canvas, 'red')
+
+while 1:
+    ball.draw()
+    tk.update_idletasks()
+    tk.update()
+    time.sleep(0.01)
